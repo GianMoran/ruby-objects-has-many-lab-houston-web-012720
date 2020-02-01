585aef64b3fb4869b9b9d6b12acf179d3b94afd7
Gian Moran <gpmoran_@outlook.com>
2020-02-01 04:13:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,9 +15,9 @@
   def add_post(post)
     post.author = self
   end
-  def add_post_by_title(name)
-   post = Post.new(name)
-   post.title = self
+  def add_post_by_title(title)
+   post = Post.new(title)
+   post.author = self
   end
   def self.post_count
   Post.all.count
